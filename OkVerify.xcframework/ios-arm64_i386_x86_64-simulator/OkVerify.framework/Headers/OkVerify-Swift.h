7af34ef251b55817374bc7c5c008360ac0fc612d
MaksymBukshovan <m.bukshovan@bioniq.com>
2021-03-11 18:04:04 +0200

    updated framework

--- OkVerify.xcframework/ios-arm64_i386_x86_64-simulator/OkVerify.framework/Headers/OkVerify-Swift.h
+++ OkVerify.xcframework/ios-arm64_i386_x86_64-simulator/OkVerify.framework/Headers/OkVerify-Swift.h
@@ -209,6 +209,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+
 SWIFT_CLASS("_TtC8OkVerify10OkHiVerify")
 @interface OkHiVerify : NSObject
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
@@ -432,6 +433,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 
+
 SWIFT_CLASS("_TtC8OkVerify10OkHiVerify")
 @interface OkHiVerify : NSObject
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
@@ -653,6 +655,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 # pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="OkVerify",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
+
 
 
 SWIFT_CLASS("_TtC8OkVerify10OkHiVerify")
